8bfd2b046b35a4e64207998238b61e790cb92777
surachet.ng <surachet.nts@gmail.com>
2024-08-29 22:25:34 +0700

    change annotation indicator

--- pkg/types/constants.go
+++ pkg/types/constants.go
@@ -2,7 +2,7 @@ package types
 
 const (
 	// Environment Variable Constants
-	AnnotationIndicator = "abc.argocd.io/path"
+	AnnotationIndicator = "ccoe-secretengine.kubernetes.io/path"
 
 	// GitLab backend
 	EnvGitLabUsername = "GITLAB_USERNAME"
